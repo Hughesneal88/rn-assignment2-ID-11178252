94c3f39d4cd2fa685d8a8505c77fd08cec8ebb64
Allison <hughesneal88@gmail.com>
2024-05-26 00:03:54 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>My name is <Text style={styles.name}>Allison</Text></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 24
+  },
+  text: {
+    fontSize: 24,
+  },
+  name:{
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
